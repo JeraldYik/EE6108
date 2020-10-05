e45a24f2755c7cc581b4ac096e37102010982fb1
Jerald Yik <jerald@ip-192-168-0-102.ap-southeast-1.compute.internal>
2020-10-05 22:30:43 +0800

    added more comments

--- dijkstra.ts
+++ dijkstra.ts
@@ -40,6 +40,54 @@ class Dijkstra {
     this.vertices[vertex.name] = vertex;
   }
 
+  // main algorithm
+  public findShortestWay(start: string, finish: string): string[] {
+    // queue : object
+    let nodes: IVertex = {};
+
+    Object.keys(this.vertices).forEach((key: string) => {
+      if (this.vertices[key].name === start) {
+        // initialise weight of start node to be 0
+        this.vertices[key].weightFromStart = 0;
+      }
+      // populate queue of nodes to be worked on
+      nodes[this.vertices[key].name] = this.vertices[key].weightFromStart;
+    });
+
+    while (Object.keys(nodes).length > 0) {
+      // sort object and make it into a queue nodes of increasing weights
+      let sortedVisitedByWeight: string[] = Object.keys(nodes).sort(
+        (a, b) =>
+          this.vertices[a].weightFromStart - this.vertices[b].weightFromStart
+      );
+
+      let currentVertex: Vertex = this.vertices[sortedVisitedByWeight[0]];
+      // for all edges (u,v) where u = currentVertex
+      currentVertex.destVertices.forEach((n: DestinationVertex) => {
+        // calculateWeight = dist(u) + l(u,v)
+        const calculateWeight: number =
+          currentVertex.weightFromStart + n.weightOfEdge;
+        // calculateWeight < dist(v)
+        if (
+          calculateWeight < this.vertices[n.nameOfDestVertex].weightFromStart
+        ) {
+          // dist(v) = calculateWeight
+          this.vertices[n.nameOfDestVertex].weightFromStart = calculateWeight;
+        }
+      });
+
+      // delete from queue
+      delete nodes[sortedVisitedByWeight[0]];
+    }
+
+    const finishWeight: number = this.vertices[finish].weightFromStart;
+    let arrayWithVertex: string[] = this.findPointsOfShortestWay(start, finish);
+    arrayWithVertex.push(finish, finishWeight.toString());
+
+    return arrayWithVertex;
+  }
+
+  // after finding shortest path from main algorithm (populating this.vertices with the appropriate weights), print out path with intermediate nodes
   private findPointsOfShortestWay(start: string, finish: string): string[] {
     let nextVertex: string = finish;
     let arrayWithVertex: string[] = [];
@@ -58,53 +106,8 @@ class Dijkstra {
       arrayWithVertex.push(minVertex);
       nextVertex = minVertex;
     }
-    return arrayWithVertex;
-  }
 
-  // main algorithm
-  public findShortestWay(start: string, finish: string): string[] {
-    let nodes: IVertex = {};
-
-    Object.keys(this.vertices).forEach((key: string) => {
-      if (this.vertices[key].name === start) {
-        // initialise weight of start node to be 0
-        this.vertices[key].weightFromStart = 0;
-      }
-      // populate object (queue) of nodes to be worked on
-      nodes[this.vertices[key].name] = this.vertices[key].weightFromStart;
-    });
-
-    while (Object.keys(nodes).length !== 0) {
-      // sort object and make it into a queue nodes of increasing weights
-      let sortedVisitedByWeight: string[] = Object.keys(nodes).sort(
-        (a, b) =>
-          this.vertices[a].weightFromStart - this.vertices[b].weightFromStart
-      );
-
-      let currentVertex: Vertex = this.vertices[sortedVisitedByWeight[0]];
-      // for all edges (u,v) where u = currentVertex
-      currentVertex.destVertices.forEach((n: DestinationVertex) => {
-        const calculateWeight: number =
-          currentVertex.weightFromStart + n.weightOfEdge;
-        if (
-          calculateWeight < this.vertices[n.nameOfDestVertex].weightFromStart
-        ) {
-          this.vertices[n.nameOfDestVertex].weightFromStart = calculateWeight;
-        }
-      });
-
-      // delete from queue
-      delete nodes[sortedVisitedByWeight[0]];
-    }
-
-    const finishWeight: number = this.vertices[finish].weightFromStart;
-    let arrayWithVertex: string[] = this.findPointsOfShortestWay(
-      start,
-      finish
-    ).reverse();
-    arrayWithVertex.push(finish, finishWeight.toString());
-
-    return arrayWithVertex;
+    return arrayWithVertex.reverse();
   }
 }
 
